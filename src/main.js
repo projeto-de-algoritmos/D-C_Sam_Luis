ea3bf8e7784865c53d9f9ae2b2749f9a14154e74
Sam Buters <sambuters@gmail.com>
2020-11-13 19:37:50 -0300

    Adding result displaying
    Co-authored-by: Luis Taira <luishptaira@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 var tierListOne = [];
 var tierListTwo = [];
 var curId = 0;
+var spinAudio;
 
 $(document).ready(() => {
 	$("form").submit(function (e) {
@@ -21,7 +22,26 @@ $(document).ready(() => {
 
 		e.preventDefault();
 	});
+	
+	let request = new XMLHttpRequest();
+	request.open("GET", "assets/spin.mp3", true);
+	request.responseType = "blob";    
+	request.onload = function() {
+		if(this.status == 200) {
+			spinAudio = new Audio(URL.createObjectURL(this.response));
+			spinAudio.load();
+		}
+	}
+	
+	request.send();
 });
+
+function deleteCard(id) {
+	$(`.${id}`).remove();
+	
+	tierListOne.splice(tierListOne.findIndex(element => element.id == id), 1);
+	tierListTwo.splice(tierListTwo.findIndex(element => element.id == id), 1);
+}
 
 function compare() {
 	const listTwoValues = [];
@@ -37,7 +57,43 @@ function compare() {
 	});
 
 	const maxInversions = ((numberOfElements * (numberOfElements - 1)) / 2);
-	const inversions = sortAndCount({array: listTwoValues, count: 0})
-	const listDifference = (( 1 - (inversions / maxInversions)) * 100.0);
+	const inversions = sortAndCount({array: listTwoValues, count: 0});
+	const listDifference = (( 1 - (inversions.count / maxInversions)) * 100.0);
 
+	showResults(listDifference);
+}
+
+function sleep(ms) {
+	return new Promise((resolve, reject) => setTimeout(() => resolve(), ms));
+}
+
+async function showResults(result) {
+	const resultDisplay = $('.result');
+	const spinNumber = 25;
+	let numberOfUselessNumbers = spinNumber;
+		
+	while(numberOfUselessNumbers) {
+		resultDisplay.text(`${(Math.random() * 100).toFixed(2)}%`);
+		spinAudio.currentTime = 0;
+		spinAudio.play();
+		await sleep(50 + 100 * (1 - numberOfUselessNumbers/spinNumber));
+		numberOfUselessNumbers -= 1;
+	}
+	
+	spinAudio.currentTime = 0;
+	spinAudio.play();
+	resultDisplay.text(`${result.toFixed(2)}%`);
+	resultDisplay.animate({
+		fontSize: '500px'
+	}, 0, () => {
+		resultDisplay.animate({fontSize: '64px'}, 1000, 'easeInCubic', () => {
+			resultDisplay.animate({fontSize: '84px'}, 500, 'easeOutCubic', () => {
+				resultDisplay.animate({fontSize: '64px'}, 500, 'easeInCubic', () => {
+					resultDisplay.animate({fontSize: '70px'}, 250, 'easeOutCubic', () => {
+						resultDisplay.animate({fontSize: '64px'}, 250, 'easeInCubic');
+					});
+				});
+			});
+		});
+	});
 }
