f6c749cd5b7448364a890eec7ac86e4360e551b6
Luis Taira <luishptaira@gmail.com>
2020-11-13 12:18:48 -0300

    Improving drag and drop and binding content to array

---
+++ src/dragAndDrop.js
@@ -0,0 +1,101 @@
+function setupDragAndDrop(curId) {
+    $(".tierlist").sortable({
+
+        axis: "y",
+        revert: true,
+        scroll: false,
+        placeholder: "sortable-placeholder",
+        cursor: "move"
+
+    });
+
+    $(".tier-card").each(function (i) {
+        var item = $(this);
+        var item_clone = item.clone();
+        item.data("clone", item_clone);
+        var position = item.position();
+        item_clone
+            .css({
+                left: position.left,
+                top: position.top,
+                visibility: "hidden"
+            })
+            .attr("data-pos", i + 1);
+
+        $("#cloned-slides").append(item_clone);
+    });
+
+    $(".tierlist").sortable({
+
+        axis: "y",
+        revert: true,
+        scroll: false,
+        placeholder: "sortable-placeholder",
+        cursor: "move",
+
+        start: function (e, ui) {
+            ui.helper.addClass("exclude-me");
+            $(".all-slides .slide:not(.exclude-me)")
+                .css("visibility", "hidden");
+            ui.helper.data("clone").hide();
+            $(".cloned-slides .slide").css("visibility", "visible");
+        },
+
+        stop: function (e, ui) {
+            $(".tierlist .tier-card.exclude-me").each(function () {
+                var item = $(this);
+                var clone = item.data("clone");
+                var position = item.position();
+
+                clone.css("left", position.left);
+                clone.css("top", position.top);
+                clone.show();
+
+                item.removeClass("exclude-me");
+            });
+
+            $(".tierlist .tier-card").each(function () {
+                var item = $(this);
+                var clone = item.data("clone");
+
+                clone.attr("data-pos", item.index());
+            });
+
+            $(".tierlist .tier-card").css("visibility", "visible");
+            $(".cloned-slides .tier-card").css("visibility", "hidden");
+        },
+
+        change: function (e, ui) {
+            $(".tierlist .tier-card:not(.exclude-me)").each(function () {
+                var item = $(this);
+                var clone = item.data("clone");
+                clone.stop(true, false);
+                var position = item.position();
+                clone.animate({
+                    left: position.left,
+                    top: position.top
+                }, 200);
+            });
+        },
+
+        update: function(event, ui) {
+            new_locations = $(this).find('.tier-card').map(function(i, el) {
+              return $(el)
+            }).get()
+            
+            const updatedListOne = [];
+            const updatedListTwo = [];
+            if(this.classList[1] == 'tierlist2') {
+                new_locations.forEach(element => {
+                    updatedListTwo.push(element[0].textContent.slice(0, -1));
+                });
+            } else {
+                new_locations.forEach(element => {
+                    updatedListOne.push(element[0].textContent.slice(0, -1));
+                });
+            }
+            
+          }
+
+    });
+}
--- src/main.js
+++ src/main.js
@@ -1,56 +1,3 @@
-function deleteCard(id) {
-	$(`.${id}`).remove();
-
-	tierListOne.splice(tierListOne.findIndex(element => element.id == id), 1);
-	tierListTwo.splice(tierListTwo.findIndex(element => element.id == id), 1);
-}
-
-function handleDragStart(e) {
-
-	this.style.opacity = '0.4';
-
-	dragSrcEl = this;
-
-	e.dataTransfer.effectAllowed = 'move';
-	e.dataTransfer.setData('text/html', this.innerHTML);
-}
-
-function handleDragEnd(e) {
-	this.style.opacity = '1';
-	let items = document.querySelectorAll(`.c${curId}`);
-	items.forEach(function (item) {
-		item.classList.remove('over');
-	});
-}
-
-function handleDragOver(e) {
-	if (e.preventDefault) {
-		e.preventDefault();
-	}
-
-	return false;
-}
-
-function handleDragEnter(e) {
-	this.classList.add('over');
-}
-
-function handleDragLeave(e) {
-	this.classList.remove('over');
-}
-
-function handleDrop(e) {
-	e.stopPropagation();
-	this.style.opacity = '1';
-
-	if (dragSrcEl !== this) {
-		dragSrcEl.innerHTML = this.innerHTML;
-		console.log(this);
-		this.innerHTML = e.dataTransfer.getData('text/html');
-	}
-
-	return false;
-}
 var tierListOne = [];
 var tierListTwo = [];
 var curId = 0;
@@ -59,22 +6,14 @@ $(document).ready(() => {
 	$("form").submit(function (e) {
 		const title = e.target[0].value;
 		const url = e.target[1].value;
-
+		
 		if (title.replace(/\s/g, '') != '') {
 			tierListOne.push({ title: title, url: url, id: curId });
 			tierListTwo.push({ title: title, url: url, id: curId });
 			let cardComponent = `<div class="tier-card c${curId}" draggable="true"><span style="background-image: url(${url});"></span><p>${title}</p><span onClick="deleteCard(${curId})">x</span></div>`;
 			$(".tierlist").append(cardComponent);
-
-			let items = document.querySelectorAll(`.c${curId}`);
-			items.forEach(function (item) {
-				item.addEventListener('dragstart', handleDragStart, false);
-				item.addEventListener('dragover', handleDragOver, false);
-				item.addEventListener('dragenter', handleDragEnter, false);
-				item.addEventListener('dragleave', handleDragLeave, false);
-				item.addEventListener('drop', handleDrop, false);
-				item.addEventListener('dragend', handleDragEnd, false);
-			});
+			
+			setupDragAndDrop(curId);
 		}
 
 		this.reset();
